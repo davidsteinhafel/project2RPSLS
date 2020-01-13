0b5d81e478aa633dd21074b19080501d458eb3dc
David steinhafel <dasteinhafel@gmail.com>
2020-01-13 14:24:13 -0600

    Added choose name method for human players. refactored my gesture list

--- RPS_Project2/Player.cs
+++ RPS_Project2/Player.cs
@@ -8,6 +8,9 @@ namespace RPS_Project2
 {
     class Player
     {
+        public string name;
+        
+        
 
 
         public List<Gesture> gestures = new List<Gesture>();
@@ -16,11 +19,15 @@ namespace RPS_Project2
 
         public Player()
         {
-            Gesture gesture1 = new Gesture("Rock");
-            Gesture gesture2 = new Gesture("Paper");
-            Gesture gesture3 = new Gesture("Scissors");
-            Gesture gesture4 = new Gesture("Spock");
-            Gesture gesture5 = new Gesture("Lizard");
+            
+
+            
+
+            Gesture gesture1 = new Rock();
+            Gesture gesture2 = new Paper();
+            Gesture gesture3 = new Scissor();
+            Gesture gesture4 = new Spock();
+            Gesture gesture5 = new Lizard();
             gestures.Add(gesture1);
             gestures.Add(gesture2);
             gestures.Add(gesture3);
@@ -36,21 +43,21 @@ namespace RPS_Project2
             switch (choice)
             {
                 case "Rock":
-                    Console.WriteLine("Rock");
+                    Console.WriteLine(gestures[0]);
                     break;
                 case "Paper":
-                    Console.WriteLine("Paper");
+                    Console.WriteLine(gestures[1]);
                     break;
                 case "Scissors":
-                    Console.WriteLine("Scissors");
+                    Console.WriteLine(gestures[2]);
                     break;
                 case "Lizard":
-                    Console.WriteLine("Lizard");
+                    Console.WriteLine(gestures[3]);
                     break;
                 case "Spock":
-                    Console.WriteLine("Spock");
+                    Console.WriteLine(gestures[4]);
                     break;
-                    Console.ReadLine();
+                    
             }
             
         }
